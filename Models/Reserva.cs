166d4d8c0503dceaba5eb42e4c694f9c8cc4bba5
Guilherme <guilhermerblc@gmail.com>
2023-09-17 16:48:19 -0300

    Models and basic test done.

---
+++ Models/Reserva.cs
@@ -0,0 +1,50 @@
+namespace DesafioHospedagemHotel.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (this.Suite.Capacidade <= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suite.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return this.Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = this.DiasReservados * this.Suite.ValorDiaria;
+            
+            if (this.DiasReservados >= 10)
+            {
+                valor *= 0.9M;
+            }
+
+            return valor;
+        }
+    }
+}
